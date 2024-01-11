92be8c86a0fb9f4a5b1c3fb0df79dc3ad64c015a
Adi Siswanto <adi15siswanto@gmail.com>
2024-01-11 17:45:52 +0700

    update

--- src/sum.js
+++ src/sum.js
@@ -10,4 +10,21 @@ const sumAll = (arrayNumber) => {
   return number;
 };
 
+export const calculate = (numbers, callback) => {
+  let total = 0;
+  for (const number of numbers) {
+    total += number;
+  }
+
+  callback(total);
+};
+
+export const calculateAndReturn = (numbers, callback) => {
+  let total = 0;
+  for (const number of numbers) {
+    total += number;
+  }
+
+  return callback(total);
+};
 export { sum, sumAll };
